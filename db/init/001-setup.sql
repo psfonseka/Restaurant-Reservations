d3c5e86564b5bf16a4da2ae1436029664925b289
psfonseka <psfonseka@gmail.com>
2020-07-20 17:17:17 -0400

    db schema improvements

--- db/init/001-setup.sql
+++ db/init/001-setup.sql
@@ -4,6 +4,15 @@
 -- But that might be overkill, especially since I don't know how much it would improve queries anyway
 -- We'll stick with Finding availabilities based on what time/region combinations are not booked
 
+CREATE TABLE timeslots (
+  timeslot TIME PRIMARY KEY,
+  time_string TEXT NOT NULL
+);
+
+CREATE TABLE dates (
+  date DATE PRIMARY KEY
+);
+
 CREATE TABLE regions (
   id SERIAL PRIMARY KEY,
   region_name TEXT NOT NULL,
@@ -14,17 +23,20 @@ CREATE TABLE regions (
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY,
-  region_id INT REFERENCES regions(id),
-  reservation_date TIMESTAMP NOT NULL
+  region_id INT NOT NULL,
+  reservation_time TIME NOT NULL,
+  reservation_date DATE NOT NULL,
+  CONSTRAINT fk_reservation_region FOREIGN KEY (region_id) REFERENCES regions(id)
 );
 
 CREATE TABLE confirmations (
   id SERIAL PRIMARY KEY,
-  reservation_id INT REFERENCES reservations(id),
+  reservation_id INT NOT NULL,
   full_name TEXT NOT NULL,
   email TEXT NOT NULL,
   phone_number TEXT NOT NULL,
   party_size INT NOT NULL,
   birthday BOOLEAN NOT NULL,
-  birthday_name TEXT
+  birthday_name TEXT,
+  CONSTRAINT fk_confirmation_reservation FOREIGN KEY (reservation_id) REFERENCES reservations(id)
 );
---
+++ db/init/002-data.sql
@@ -0,0 +1,18 @@
+INSERT INTO timeslots (timeslot, time_string) VALUES ('6:00 PM', '6:00 PM');
+INSERT INTO timeslots (timeslot, time_string) VALUES ('6:30 PM', '6:30 PM');
+INSERT INTO timeslots (timeslot, time_string) VALUES ('7:00 PM', '7:00 PM');
+INSERT INTO timeslots (timeslot, time_string) VALUES ('7:30 PM', '7:30 PM');
+INSERT INTO timeslots (timeslot, time_string) VALUES ('8:00 PM', '8:00 PM');
+INSERT INTO timeslots (timeslot, time_string) VALUES ('8:30 PM', '8:30 PM');
+INSERT INTO timeslots (timeslot, time_string) VALUES ('9:00 PM', '9:00 PM');
+INSERT INTO timeslots (timeslot, time_string) VALUES ('9:30 PM', '9:30 PM');
+INSERT INTO timeslots (timeslot, time_string) VALUES ('10:00 PM', '10:00 PM');
+
+INSERT INTO dates (date) VALUES ('07-24-2020');
+INSERT INTO dates (date) VALUES ('07-25-2020');
+INSERT INTO dates (date) VALUES ('07-26-2020');
+INSERT INTO dates (date) VALUES ('07-27-2020');
+INSERT INTO dates (date) VALUES ('07-28-2020');
+INSERT INTO dates (date) VALUES ('07-29-2020');
+INSERT INTO dates (date) VALUES ('07-30-2020');
+INSERT INTO dates (date) VALUES ('07-31-2020');
