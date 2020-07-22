16bc1d499affc72cdd6292a4891256827dee09fc
psfonseka <psfonseka@gmail.com>
2020-07-21 21:46:54 -0400

    clean dist

--- client/dist/index.js
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from "react-dom";
-import App from './components/App';
-var mountNode = document.getElementById("app");
-ReactDOM.render(React.createElement(App, null), mountNode);
-//# sourceMappingURL=index.js.map
