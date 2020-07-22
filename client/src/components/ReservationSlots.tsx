85819886329041c97e720a1eeff177188be8c185
psfonseka <psfonseka@gmail.com>
2020-07-22 17:44:47 -0400

    display reservation slots

--- client/src/components/ReservationSlots.tsx
+++ client/src/components/ReservationSlots.tsx
@@ -8,9 +8,22 @@ interface Props {
 }
 
 const ReservationSlots = (props: Props) => {
+  console.log(Object.keys(props.reservationSlots));
   return (
     <div className="reservationSlots">
       <h2>{props.regionName} Reservation Slots</h2>
+      <div className="dates">
+        {Object.keys(props.reservationSlots).map((date) => {
+          return <div>{date}</div>
+        })}
+      </div>
+      <div className="slotsContainer">
+        {Object.keys(props.reservationSlots).map((date) => {
+          return props.reservationSlots[date].map((slot) => {
+          return <div className="slot">{slot.time}</div>
+          })
+        })}
+      </div>
     </div>
   )
 };
