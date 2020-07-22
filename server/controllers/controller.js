c934dc0d79999f6d085189ab19c97860996adb11
psfonseka <psfonseka@gmail.com>
2020-07-22 11:56:27 -0400

    created much more efficient query/get reservations slots method

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,6 +1,7 @@
 const db = require('../../db/db');
 
-db.any('select timeslots.time_string, sub.date from timeslots right join (select timeslot, date from timeslots, dates except select reservation_time, reservation_date from reservations where region_id = 2) as sub on timeslots.timeslot = sub.timeslot;')
+//db.any('select timeslots.time_string, sub.date from timeslots right join (select timeslot, date from timeslots, dates except select reservation_time, reservation_date from reservations where region_id = 2) as sub on timeslots.timeslot = sub.timeslot;')
+db.any('SELECT timeslots.time_string, dates.date, CASE WHEN reservations.reservation_time = timeslots.timeslot AND reservations.reservation_date = dates.date THEN TRUE ELSE FALSE END as taken FROM timeslots CROSS JOIN dates LEFT JOIN reservations ON reservations.reservation_time = timeslots.timeslot AND reservations.reservation_date = dates.date;')  
   .then((data) => {
     console.log(data.length);
   })
