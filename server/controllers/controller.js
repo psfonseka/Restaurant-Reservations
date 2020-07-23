2d9993198745de066b48ac4d465e783853b3c43a
psfonseka <psfonseka@gmail.com>
2020-07-22 22:06:30 -0400

    variable cleanup

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,20 +2,14 @@ const db = require('../../db/db');
 
 module.exports = {
   confirmReservation: (req, res) => {
-    console.log(req.body);
     const body = req.body
-    const region_id = body.regionInfo.id;
-    const reservation_time = body.reservationTime;
-    const reservation_date = body.reservationDate;
-    console.log(region_id, reservation_time, reservation_date);
     const guest = body.guestInfo;
     const birthday = guest.hasBirthday ? [`birthday, birthday_name`, `TRUE, '${guest.birthdayName}'`] : [`birthday`, `FALSE`];
     const children = guest.hasChildren ? [`children, number_of_children`,`TRUE, ${guest.childrenNumber}`] : [`children`, `FALSE`];
     db.any(`INSERT INTO reservations (region_id, reservation_time, reservation_date) 
-      VALUES (${region_id}, '${reservation_time}', '${reservation_date}') RETURNING id;`)
+      VALUES (${body.regionInfo.id}, '${body.reservationTime}', '${body.reservationDate}') RETURNING id;`)
       .then((reservation_id) => {
         const id = reservation_id[0].id;
-        console.log(id);
         return db.any(`INSERT INTO confirmations (reservation_id, full_name, email, phone_number, party_size, ${birthday[0]}, ${children[0]}, smoking) 
           VALUES (${id}, '${guest.fullName}', '${guest.email}', '${guest.phoneNumber}', ${guest.partySize}, ${birthday[1]}, ${children[1]}, ${guest.hasSmoker});`)
       })
