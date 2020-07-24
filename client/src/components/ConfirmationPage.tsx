4af974cf9464961fe7f3192b67e0d4a3fc7f2165
psfonseka <psfonseka@gmail.com>
2020-07-24 13:38:01 -0400

    confirmation page improvements

--- client/src/components/ConfirmationPage.tsx
+++ client/src/components/ConfirmationPage.tsx
@@ -11,20 +11,24 @@ const ConfirmationPage = (props: Props) => {
     <div className="confirmationPage">
       {props.info.confirmed && 
       <h3>Thank you! You can view your reservation below:</h3>}
-      <h2>Confirmation</h2>
-      <div>Reservation Time: {props.info.reservationTime}</div>
-      <div>Reservation Date: {props.info.reservationDate}</div>
-      <div>Guest Name: {props.info.guestInfo?.fullName}</div>
-      <div>E-mail: {props.info.guestInfo?.email}</div>
-      <div>Phone Number: {props.info.guestInfo?.phoneNumber}</div>
-      <div>Dining Region: {props.info.regionInfo?.region_name}</div>
-      <div>Number of Guests: {props.info.guestInfo?.partySize}</div>
-      {props.info.guestInfo?.hasChildren && 
-      <div>Number of Children: {props.info.guestInfo.childrenNumber}</div>}
-      {props.info.guestInfo?.hasBirthday && 
-      <div>Birthday Name: {props.info.guestInfo.birthdayName}</div>}
       {!props.info.confirmed && 
-      <button onClick={props.confirmReservation}>Confirm Reservation</button>}
+      <h3>Confirmation</h3>}
+      <div className="confirmationInfo">
+        <div>Reservation Time: {props.info.reservationTime}</div>
+        <div>Reservation Date: {props.info.reservationDate}</div>
+        <div>Guest Name: {props.info.guestInfo?.fullName}</div>
+        <div>E-mail: {props.info.guestInfo?.email}</div>
+        <div>Phone Number: {props.info.guestInfo?.phoneNumber}</div>
+        <div>Dining Region: {props.info.regionInfo?.region_name}</div>
+        <div>Number of Guests: {props.info.guestInfo?.partySize}</div>
+        {props.info.guestInfo?.hasChildren && 
+        <div>Number of Children: {props.info.guestInfo.childrenNumber}</div>}
+        {props.info.guestInfo?.hasBirthday && 
+        <div>Birthday Name: {props.info.guestInfo.birthdayName}</div>}
+        <br/>
+      </div>
+      {!props.info.confirmed && 
+      <button className="button" onClick={props.confirmReservation}>Confirm Reservation</button>}
     </div>
   )
 };
